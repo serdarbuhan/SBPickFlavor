193200384c3699c63cbfeee4a27190b4c1d42c08
Ahmet Serdar Buhan <ahmet.buhan@verizonconnect.com>
2020-07-30 16:15:59 +0100

    Initial commit

---
+++ Pods/Target Support Files/Pods-SBPickFlavor/Pods-SBPickFlavor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SBPickFlavor : NSObject
+@end
+@implementation PodsDummy_Pods_SBPickFlavor
+@end
---
+++ SBPickFlavor/SBPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  SBPickFlavor.h
+//  SBPickFlavor
+//
+//  Created by Ahmet Buhan on 7/29/20.
+//  Copyright © 2020 asdff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SBPickFlavor.
+FOUNDATION_EXPORT double SBPickFlavorVersionNumber;
+
+//! Project version string for SBPickFlavor.
+FOUNDATION_EXPORT const unsigned char SBPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBPickFlavor/PublicHeader.h>
+
+
